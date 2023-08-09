ece32d0c1142ab556781d9a27070714f9704e7a9
Bobby yadav <105139819+bobbypen@users.noreply.github.com>
2023-08-09 07:59:45 +0530

    Add files via upload

---
+++ _1_time_compx.java
@@ -0,0 +1,19 @@
+package lecture13;
+
+public class _1_time_compx {
+
+	//this is experimental way 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+		
+	}
+
+}
